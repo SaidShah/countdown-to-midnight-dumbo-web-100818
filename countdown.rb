630646858639c916c8a1a533185bf936826bffb3
saidmysr20@gmail.com <saidmysr20@gmail.com>
2018-08-14 00:01:00 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -3,9 +3,12 @@
 def countdown(given_time)
   while given_time > 0 do 
     puts "#{given_time} SECOND(S)!"
+    sleep_for_me(1)
     given_time-=1 
   end
   return "HAPPY NEW YEAR!"
 end
 
-
+def sleep_for_me(given_seconds)
+  sleep(given_seconds)
+end
